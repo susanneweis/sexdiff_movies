8a2d5e013077cf7af2d2bf654d290986deda73e5
Susanne Weis <s.weis@fz-juelich.de>
2025-11-12 11:57:42 +0100

    started on run_sTOPF

---
+++ run_sTOPF/2b_sTOPF_individual_expressions.py
@@ -0,0 +1,157 @@
+import pandas as pd
+import numpy as np
+import os
+from scipy.stats import pearsonr
+import statsmodels.api as sm
+from sklearn.feature_selection import mutual_info_regression
+
+def main(): 
+    # Local setup for testing 
+    # for Juseless Version see Kristina's code: PCA_foreachsex_allROI_latestversion.py
+
+    base_path =  "/Users/sweis/Data/Arbeit/Juseless/data/project/brainvar_sexdiff_movies" 
+    data_path = f"{base_path}/data_pipeline_sTOPF"
+
+    ### change!!!!
+    results_path = f"{base_path}/results_pipeline_sTOPF"
+    ind_path = f"{results_path}/individual_expressions"
+    os.makedirs(ind_path, exist_ok=True)
+
+    phenotype_path = f"{data_path}/Participant_sex_info.csv"
+    complete_participants_path = f"{data_path}/complete_participants.csv"
+    excluded_participants_path = f"{data_path}/excluded_participants.csv"
+
+    # not relevant yet, as currently not considering hormones
+    # exclude_path = f"{base_path}/results_pipeline/excluded_subjects.csv"
+
+    sex_mapping = {1: 'male', 2: 'female'}
+    subs_sex = pd.read_csv(f"{data_path}/Participant_sex_info.csv", sep = ";")
+    subs_sex['gender'] = subs_sex['gender'].replace(sex_mapping)
+
+    # Define movie timepoint parameters
+    movies_properties = {
+        "dd": {"min_timepoint": 6, "max_timepoint": 463},
+        "s": {"min_timepoint": 6, "max_timepoint": 445},
+        "dps": {"min_timepoint": 6, "max_timepoint": 479},
+        "fg": {"min_timepoint": 6, "max_timepoint": 591},
+        "dmw": {"min_timepoint": 6, "max_timepoint": 522},
+        "lib": {"min_timepoint": 6, "max_timepoint": 454},
+        "tgtbtu": {"min_timepoint": 6, "max_timepoint": 512},
+        "rest_run-1": {"min_timepoint": 6, "max_timepoint": 499},
+        "rest_run-2": {"min_timepoint": 6, "max_timepoint": 499}
+    }
+
+    movies = ["dd", "s", "dps", "fg", "dmw", "lib", "tgtbtu", "rest_run-1", "rest_run-2"]
+
+    # Load phenotype data (assumed to be a CSV with a subject ID and gender columns)
+    phenotypes = pd.read_csv(phenotype_path, sep=';')
+    phenotypes.columns = ['subject_ID', 'gender']
+
+    # Load list of complete participants (verified list with participants_verification.py)
+    complete_participants = set(pd.read_csv(complete_participants_path)['subject'].astype(str))
+    excluded_participants = set(pd.read_csv(excluded_participants_path)['subject'].astype(str))
+
+    # Load list of excluded subjects (hormonal outlier detection with hormone_outlier_detection_SD.py)
+    # not yet relevant here 
+    # exclude_df = pd.read_csv(exclude_path, sep=',')
+    # excluded_subjects = set(exclude_df['PCode'].astype(str))
+
+    # Get valid subjects and exclude outliers
+    phenotype_subjects = set(phenotypes['subject_ID'].astype(str))
+    valid_subjects = complete_participants.intersection(phenotype_subjects)
+    valid_subjects = valid_subjects.difference(excluded_participants)
+
+    # not yet relevant here
+    # valid_subjects = valid_subjects.difference(excluded_subjects)
+
+    print(f"Number of included valid subjects after exclusion: {len(valid_subjects)}")
+
+    loo_results_all = []
+
+    for subj in valid_subjects:
+
+        loo_results_subj = []
+
+        for curr_mov in movies:
+            dataset = f"BOLD_Schaefer400_subcor36_mean_task-{curr_mov}_MOVIES_INM7.csv"
+            movie_path =  f"{data_path}/fMRIdata/{dataset}" # Path to fMRI data
+
+            properties = movies_properties[curr_mov] # Get timepoint properties for the movie
+            
+            # Load fMRI data
+            movie_data = pd.read_csv(movie_path)
+            if "Unnamed: 0" in movie_data.columns:
+                movie_data = movie_data.drop(columns=["Unnamed: 0"]) # Drop unnecessary columns
+                
+            # Define column names and brain regions
+            brain_regions = movie_data.columns[2:]  # Extract all brain region columns (assuming the first two columns are not brain regions) 
+
+            # Filter timepoints based on movie properties
+            movie_data = movie_data[
+                (movie_data["timepoint"] >= properties["min_timepoint"]) & 
+                (movie_data["timepoint"] <= properties["max_timepoint"])
+            ] 
+            print(f"movie properties {curr_mov}", movie_data["timepoint"].min(), movie_data["timepoint"].max(),"\n") 
+            
+            subj_movie_data = movie_data.loc[movie_data["subject"] == subj].copy()
+
+            # Define the output directory
+            # if hostname == "cpu44":
+            #   output_dir =r_rootdir # Remote root directory
+            #else:
+
+            pca_path = f"{results_path}/results_PCA/{curr_mov}/{subj}"
+            pca_scores_female = pd.read_csv(f"{pca_path}/PC1_scores_female_allROI.csv")
+            pca_scores_male=  pd.read_csv(f"{pca_path}/PC1_scores_male_allROI.csv")
+
+            for region in brain_regions:        
+                
+                pca_fem = pca_scores_female.loc[pca_scores_female["Region"] == region, "PC_score_1"]
+                pca_mal = pca_scores_male.loc[pca_scores_male["Region"] == region, "PC_score_1"]
+
+                rf, p = pearsonr(subj_movie_data[region], pca_fem)
+                rm, p = pearsonr(subj_movie_data[region], pca_mal)
+
+                diff = np.arctanh(rf) - np.arctanh(rm)
+                diff = np.tanh(diff)
+
+                # standardize
+                y = (subj_movie_data[region] - np.mean(subj_movie_data[region])) / np.std(subj_movie_data[region])
+                xf = (pca_fem - np.mean(pca_fem)) / np.std(pca_fem)
+                xm = (pca_mal - np.mean(pca_mal)) / np.std(pca_mal)
+
+                # design matrix
+                X = np.column_stack([xf, xm])
+                X = sm.add_constant(X)
+                model = sm.OLS(y, X).fit()
+
+                beta_f, beta_m = model.params[1], model.params[2]
+                fem_similarity = (beta_f - beta_m) / (abs(beta_f) + abs(beta_m))
+
+                # mutual information
+                df_y = pd.DataFrame(y)
+                df_xf = pd.DataFrame(xf)
+                df_xm = pd.DataFrame(xm)
+
+                mi_f = mutual_info_regression(df_y, df_xf, random_state=42)
+                mi_m = mutual_info_regression(df_y, df_xm, random_state=42)
+
+                
+                sub_sex = subs_sex.loc[subs_sex["subject_ID"] == subj, "gender"].iloc[0]
+
+                loo_results_all.append({"subject": subj, "sex": sub_sex, "movie": curr_mov, "region": region, "correlation_female": rf, "correlation_male": rm, "femaleness": diff, "fem_similarity": fem_similarity, "fem_mi": mi_f[0], "mal_mi": mi_m[0]})
+                loo_results_subj.append({"subject": subj, "sex": sub_sex, "movie": curr_mov, "region": region, "correlation_female": rf, "correlation_male": rm, "femaleness": diff, "fem_similarity": fem_similarity, "fem_mi": mi_f[0], "mal_mi": mi_m[0]})
+            
+        out_df = pd.DataFrame(loo_results_subj, columns=["subject","sex","movie","region","correlation_female","correlation_male","femaleness","fem_similarity","fem_mi","mal_mi"])
+        out_csv = f"{ind_path}/individual_expression_{subj}.csv"
+        out_df.to_csv(out_csv, index=False)
+        print(f"Saved: {out_csv}")
+
+    out_df = pd.DataFrame(loo_results_all, columns=["subject","sex","movie","region","correlation_female","correlation_male","femaleness","fem_similarity","fem_mi","mal_mi"])
+    out_csv = f"{results_path}/individual_expression_all.csv"
+    out_df.to_csv(out_csv, index=False)
+    print(f"Saved: {out_csv}")
+
+# Execute script
+if __name__ == "__main__":
+    main()
---
+++ run_sTOPF/3_sTOPF_analyse_results.py
@@ -0,0 +1,153 @@
+import pandas as pd
+
+def main():
+
+    base_path =  "/Users/sweis/Data/Arbeit/Juseless/data/project/brainvar_sexdiff_movies" 
+    results_path = f"{base_path}/results_pipeline_sTOPF"
+    
+    # Change this later 
+    ind_expr_path = f"{results_path}/individual_expression_all.csv"
+    ind_expr = pd.read_csv(ind_expr_path)
+
+    movies = ["dd", "s", "dps", "fg", "dmw", "lib", "tgtbtu", "rest_run-1", "rest_run-2"]
+    regions = ind_expr["region"].astype(str).drop_duplicates().tolist()
+    
+    # res_summary = []
+
+    ind_expr["class_corr"] = (
+        ((ind_expr["sex"] == "female") & (ind_expr["femaleness"] >= 0)) |
+        ((ind_expr["sex"] == "male") & (ind_expr["femaleness"] < 0))
+    )
+
+    ind_expr["class_corr_sim"] = (
+        ((ind_expr["sex"] == "female") & (ind_expr["fem_similarity"] >= 0)) |
+        ((ind_expr["sex"] == "male") & (ind_expr["fem_similarity"] < 0))
+    )
+    ind_expr.to_csv(f"{results_path}/correct_classification_femaleness.csv", index=False)
+
+    ind_expr["class_corr_mi"] = (
+        ((ind_expr["sex"] == "female") & (ind_expr["fem_mi"] >= ind_expr["mal_mi"] )) |
+        ((ind_expr["sex"] == "male") & (ind_expr["fem_mi"] < ind_expr["mal_mi"]))
+    )
+    ind_expr.to_csv(f"{results_path}/correct_classification_mi.csv", index=False)
+
+    movie_class_summary = []
+
+    for curr_mov in movies:
+        
+        mv_class = ind_expr.loc[ind_expr["movie"] == curr_mov, ["sex","class_corr","class_corr_sim","class_corr_mi"]].reset_index(drop=True)
+
+        mv_class_fem = mv_class.loc[mv_class["sex"] == "female", ["sex","class_corr","class_corr_sim","class_corr_mi"]].reset_index(drop=True)
+        mv_class_mal = mv_class.loc[mv_class["sex"] == "male", ["sex","class_corr","class_corr_sim","class_corr_mi"]].reset_index(drop=True)
+        nr_fem = len(mv_class_fem)
+        nr_mal = len(mv_class_mal)
+
+        count_true_fem = mv_class_fem["class_corr"].sum()
+        count_true_mal = mv_class_mal["class_corr"].sum()
+
+        count_true_fem_sim = mv_class_fem["class_corr_sim"].sum()
+        count_true_mal_sim = mv_class_mal["class_corr_sim"].sum()
+    
+        count_true_fem_mi = mv_class_fem["class_corr_mi"].sum()
+        count_true_mal_mi = mv_class_mal["class_corr_mi"].sum()
+
+        movie_class_summary.append({"movie": curr_mov, "female corr femaleness": count_true_fem/nr_fem, "male corr femaleness": count_true_mal/nr_mal, "female corr fem_sim": count_true_fem_sim/nr_fem, "male corr fem_sim": count_true_mal_sim/nr_mal, "female corr mi": count_true_fem_mi/nr_fem, "male corr mi": count_true_mal_mi/nr_mal})
+
+    movie_class_summary_df = pd.DataFrame(movie_class_summary)
+    movie_class_summary_df.to_csv(f"{results_path}/correct_classification_per_movie.csv", index=False)
+
+    region_class_summary = []
+    for curr_reg in regions:
+        reg_class = ind_expr.loc[ind_expr["region"] == curr_reg, ["sex","class_corr","class_corr_sim","class_corr_mi"]].reset_index(drop=True)
+
+        reg_class_fem = reg_class.loc[reg_class["sex"] == "female", ["sex","class_corr","class_corr_sim","class_corr_mi"]].reset_index(drop=True)
+        reg_class_mal = reg_class.loc[reg_class["sex"] == "male", ["sex","class_corr","class_corr_sim","class_corr_mi"]].reset_index(drop=True)
+        nr_fem = len(reg_class_fem)
+        nr_mal = len(reg_class_mal)
+
+        count_true_fem_r = reg_class_fem["class_corr"].sum()
+        count_true_mal_r = reg_class_mal["class_corr"].sum()
+
+        count_true_fem_r_sim = reg_class_fem["class_corr_sim"].sum()
+        count_true_mal_r_sim = reg_class_mal["class_corr_sim"].sum()
+
+        count_true_fem_r_mi = reg_class_fem["class_corr_mi"].sum()
+        count_true_mal_r_mi = reg_class_mal["class_corr_mi"].sum()
+
+        region_class_summary.append({"region": curr_reg, "female corr femaleness": count_true_fem_r/nr_fem, "male corr femaleness": count_true_mal_r/nr_mal, "female corr fem_sim": count_true_fem_r_sim/nr_fem, "male corr fem_sim": count_true_mal_r_sim/nr_mal, "female corr mi": count_true_fem_r_mi/nr_fem, "male corr mi": count_true_mal_r_mi/nr_mal})
+
+    region_class_summary_df = pd.DataFrame(region_class_summary)
+    region_class_summary_df.to_csv(f"{results_path}/correct_classification_per_region.csv", index=False)
+
+    act_movies = ["dd", "s", "dps", "fg", "dmw", "lib", "tgtbtu"]
+    act_mv_region_class_summary = []
+    for curr_reg in regions:
+        reg_class = ind_expr.loc[ind_expr["region"] == curr_reg, ["sex","movie","class_corr","class_corr_sim","class_corr_mi"]].reset_index(drop=True)
+        reg_class = reg_class[reg_class["movie"].isin(act_movies)]
+
+        reg_class_fem = reg_class.loc[reg_class["sex"] == "female", ["sex","movie","class_corr","class_corr_sim","class_corr_mi"]].reset_index(drop=True)
+        reg_class_mal = reg_class.loc[reg_class["sex"] == "male", ["sex","movie","class_corr","class_corr_sim","class_corr_mi"]].reset_index(drop=True)
+        nr_fem = len(reg_class_fem)
+        nr_mal = len(reg_class_mal)
+
+        count_true_fem_r = reg_class_fem["class_corr"].sum()
+        count_true_mal_r = reg_class_mal["class_corr"].sum()
+
+        count_true_fem_r_sim = reg_class_fem["class_corr_sim"].sum()
+        count_true_mal_r_sim = reg_class_mal["class_corr_sim"].sum()
+    
+        count_true_fem_r_mi = reg_class_fem["class_corr_mi"].sum()
+        count_true_mal_r_mi = reg_class_mal["class_corr_mi"].sum()
+
+        act_mv_region_class_summary.append({"region": curr_reg, "female corr femaleness": count_true_fem_r/nr_fem, "male corr femaleness": count_true_mal_r/nr_mal, "female corr fem_sim": count_true_fem_r_sim/nr_fem, "male corr fem_sim": count_true_mal_r_sim/nr_mal, "female corr mi": count_true_fem_r_mi/nr_fem, "male corr mi": count_true_mal_r_mi/nr_mal})
+
+    act_mv_region_class_summary_df = pd.DataFrame(act_mv_region_class_summary)
+    act_mv_region_class_summary_df.to_csv(f"{results_path}/correct_classification_per_region_no_rest.csv", index=False)
+
+    mv_reg_class_summary = []
+    for curr_reg in regions:
+        reg_class = ind_expr.loc[ind_expr["region"] == curr_reg, ["sex","movie","class_corr","class_corr_sim","class_corr_mi"]].reset_index(drop=True)
+
+        for curr_mov in movies: 
+
+            reg_class = ind_expr.loc[ind_expr["movie"] == curr_mov, ["sex","class_corr","class_corr_sim","class_corr_mi"]].reset_index(drop=True)
+
+            reg_class_fem = reg_class.loc[reg_class["sex"] == "female", ["sex","class_corr","class_corr_sim","class_corr_mi"]].reset_index(drop=True)
+            reg_class_mal = reg_class.loc[reg_class["sex"] == "male", ["sex","class_corr","class_corr_sim","class_corr_mi"]].reset_index(drop=True)
+            nr_fem = len(reg_class_fem)
+            nr_mal = len(reg_class_mal)
+
+            count_true_fem_r = reg_class_fem["class_corr"].sum()
+            count_true_mal_r = reg_class_mal["class_corr"].sum()
+
+            count_true_fem_r_sim = reg_class_fem["class_corr_sim"].sum()
+            count_true_mal_r_sim = reg_class_mal["class_corr_sim"].sum()
+
+            count_true_fem_r_mi = reg_class_fem["class_corr_mi"].sum()
+            count_true_mal_r_mi = reg_class_mal["class_corr_mi"].sum()
+
+            mv_reg_class_summary.append({"region": curr_reg, "movie": curr_mov, "female corr femaleness": count_true_fem_r/nr_fem, "male corr femaleness": count_true_mal_r/nr_mal, "female corr fem_sim": count_true_fem_r_sim/nr_fem, "male corr fem_sim": count_true_mal_r_sim/nr_mal,  "female corr mi": count_true_fem_r_mi/nr_fem, "male corr mi": count_true_mal_r_mi/nr_mal})
+
+    mv_reg_class_summary_df = pd.DataFrame(mv_reg_class_summary)
+    mv_reg_class_summary_df.to_csv(f"{results_path}/correct_classification_per_region_per_movie.csv", index=False)
+
+    ind_expr = pd.read_csv(ind_expr_path)
+    
+    # Aggregate per subject+movie
+    out = (
+        ind_expr.groupby(["subject", "sex", "movie"], as_index=False)
+        .agg(
+            mean_femaleness=("femaleness", "mean"),                 # NaNs ignored by default
+            mean_fem_similarity=("fem_similarity", "mean"),                 # NaNs ignored by default
+            neg_perc =("femaleness", lambda s: (s < 0).sum()/436) # NaNs don't count as negative
+        )
+        .sort_values(["subject", "movie"])
+    )
+
+    # Save result
+    out.to_csv(f"{results_path}/subject_movie_summary.csv", index=False)
+
+
+# Execute script
+if __name__ == "__main__":
+    main()
