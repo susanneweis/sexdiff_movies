9ad9e736a533b7777d3ebb2ec25fab10a2b6b78d
Susanne Weis <s.weis@fz-juelich.de>
2026-01-01 21:06:33 +0100

    read in error

--- run_sTOPF/_2b_sTOPF_individual_expressions.py
+++ run_sTOPF/_2b_sTOPF_individual_expressions.py
@@ -26,7 +26,7 @@ def main(base_path,proj,nn_mi,movies_properties):
     # exclude_path = f"{base_path}/results_pipeline/excluded_subjects.csv"
 
     sex_mapping = {1: 'male', 2: 'female'}
-    subs_sex = pd.read_csv(f"{data_path}/Participant_sex_info.csv", sep = ";")
+    subs_sex = pd.read_csv(f"{data_path}/Participant_sex_info.csv")
     subs_sex['gender'] = subs_sex['gender'].replace(sex_mapping)
 
     # Define movie timepoint parameters
